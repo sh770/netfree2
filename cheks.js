09f5e8b5fe6c6e8513d62dedf3de4e95d985e6e4
Shalom Harpaz <sh770h@gmail.com>
2023-08-04 00:27:36 +0300

    בי

--- cheks.js
+++ cheks.js
@@ -4,11 +4,11 @@ let eee = document.referrer
 console.log(eee);
 
 function verifyReferrer(){
-  if (eee === "https://remove-wifree.gq/payment.html") {
+  if (eee === "https://remove-wifree.gq/payment") {
     //   alert('הגעת ממקור תקין.');
 
     // כתובת המקור המאושרת
-    let approvedReferrer = "https://remove-wifree.gq/payment.html";
+    let approvedReferrer = "https://remove-wifree.gq/payment";
 
     // בדיקה האם המקור הוא המאושר
     if (document.referrer === approvedReferrer) {
@@ -17,7 +17,7 @@ function verifyReferrer(){
       // alert('הערך נוסף ללוקאל סטורג.');
       // העברה לדף ההורדה
       console.log("1");
-      window.location.href = "https://remove-wifree.gq/downloads.html";
+      window.location.href = "https://remove-wifree.gq/downloads";
     }
   } else {
     console.log("error");
