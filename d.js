d62ee4ba6bd7c6fff90ecb09494f00879a1172cb
Shalom Harpaz <sh770h@gmail.com>
2023-08-04 01:14:33 +0300

    dd

--- d.js
+++ d.js
@@ -1,5 +1,5 @@
 function downloadFiles() {
   window.location.href = "https://remove-wifree.gq/images.rar";
   localStorage.clear();
-  window.location.href = "downloads.html"; // מעבר לדף הבית 
+  // window.location.href = "downloads.html"; // מעבר לדף הבית 
 }
